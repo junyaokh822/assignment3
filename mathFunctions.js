b5806ed608214e746c73509c3b1667dafce09327
junyaokh822 <junyaokh822@gmail.com>
2023-06-20 13:52:22 -0400

    fixed the error of compiling

--- mathFunctions.js
+++ mathFunctions.js
@@ -1,8 +1,21 @@
-const mathFunctions = require('./mathFunctions');
- 
-test('mathFunctions(1, 2) takes in two inputs and perform add, subtract, multiply, and divide', () => {
-  expect(sum(1, 2)).toBe(3);
-  expect(subtract(1, 2)).toBe(-1);
-  expect(multiply(1, 2)).toBe(2);
-  expect(divide(1, 2)).toBe(0.5);
-});
+function add(a, b) {
+  return a + b;
+}
+
+
+function subtract(a, b) {
+  return a - b;
+}
+
+
+function multiply(a, b) {
+  return a * b;
+}
+
+
+function divide(a, b) {
+  return a / b;
+}
+module.exports= {add, subtract, multiply,divide};
+
+
--- mathFunctions.test.js
+++ mathFunctions.test.js
@@ -1,19 +1,25 @@
-function add(a, b) {
-    return a + b;
-}
-module.exports.add = add;
+const {add, subtract, multiply, divide} = require('./mathFunctions');
+describe("add(1, 2) takes in two inputs and perform add", () => {
+    test("add(1,2)=> 3", () => {
+    expect(add(1, 2)).toBe(3);
+})
+});
 
-function subtract(a, b) {
-    return a - b;
-}
-module.exports.subtract = subtract;
+describe("subtract(1, 2) takes in two inputs and perform subtract", () => {
+    test("subtract(1,2)=> -1", () => {
+    expect(subtract(1, 2)).toEqual(-1);
+})
+});
 
-function multiply(a, b) {
-    return a * b;
-}
-module.exports.multiply = multiply;
+describe("multiply(1, 2) takes in two inputs and perform multiply", () => {
+    test("multiply(1,2)=> 2", () => {
+    expect(multiply(1, 2)).toEqual(2);
+})
+});
 
-function divide(a, b) {
-    return a / b;
-}
-module.exports.divide = divide;
+describe("divide(1, 2) takes in two inputs and perform divide", () => {
+    test("divide(1,2)=> 0.5", () => {
+    expect(divide(1, 2)).toEqual(0.5);
+})
+});
+  
