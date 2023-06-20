3b49315a1fc171b8037d5e5ba885811ca42f065b
junyaokh822 <junyaokh822@gmail.com>
2023-06-20 16:15:06 -0400

    lengthOfLongestSubstring done

--- lengthOfLongestSubstring.js
+++ lengthOfLongestSubstring.js
@@ -1,15 +1,22 @@
-function lengthOfLongestSubstring(string){
-    let max = 0;
-    let start = 0;
-    let end = 0;
+function lengthOfLongestSubstring(string) {
+    let max = 0; // Variable to store the maximum length of the substring without repeating characters
+    let start = 0; // Start index of the current substring
+    let end = 0; // End index of the current substring
+    const visited = new Set(); // Set to keep track of visited characters in the current substring
 
-    while(end < string.length){
-        if(string[end]!== string[start]){
-            max = Math.max(max, end - start + 1);
-            start++;
+    while (end < string.length) {
+        if (!visited.has(string[end])) { // If the current character is not visited
+            visited.add(string[end]); // Add the current character to the visited set
+            max = Math.max(max, visited.size); // Update max if the current substring length is greater
+            end++; // Move the end index to the next character
+        } else {
+            visited.delete(string[start]); // Remove the character at the start index from the visited set
+            start++; // Move the start index to the next character
         }
-        end++;
     }
 
-    return max;
+    return max; // Return the maximum length of the substring without repeating characters
 }
+
+module.exports = lengthOfLongestSubstring;
+
---
+++ lengthOfLongestSubstring.test.js
@@ -0,0 +1,15 @@
+const lengthOfLongestSubstring = require('./lengthOfLongestSubstring');
+
+describe("lengthOfLongestSubstring(string) find the length of the longest substring without repeating characters", () => {
+    test("lengthOfLongestSubstring(abcabcbb) => 3", () => {
+        expect(lengthOfLongestSubstring("abcabcbb")).toEqual(3);
+    });
+
+    test("lengthOfLongestSubstring(bbbbb) => 1", () => {
+        expect(lengthOfLongestSubstring("bbbbb")).toEqual(1);
+    });
+
+    test("lengthOfLongestSubstring(pwwkew) => 3", () => {
+        expect(lengthOfLongestSubstring("pwwkew")).toEqual(3);
+    });
+});
